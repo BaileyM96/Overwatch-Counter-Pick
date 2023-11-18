2be0c29637b5432ffcd58ca50f29f24ed7df3443
Bailey Mejia <baileymejia28@gmail.com>
2023-11-18 12:06:57 -0800

    changed the icon to svg so its more clear and changed the flavicon

--- src/components/CharacterSelect.js
+++ src/components/CharacterSelect.js
@@ -1,16 +1,14 @@
-//Add the overwatch Image to this component
-import overwatchlogo  from '../images/overwatchlogo.png';
 import Card from '../components/Cards/Card';
 import { TitleContainer } from './styled/Containers/Container.styled';
-import Navbar from './Navbar/Navbar';
+
 
 export default function CharacterSelect() {
     return (
         <>
         <TitleContainer>
-            <img src={overwatchlogo} width='89px' height='89px' alt='overwatch logo'/>
-            <h1 style={{color: 'white'}}>Overwatch Counter</h1>
-            {/* <h3 style={{color: 'white'}}>Choose who you want to counter</h3> */}
+            <img src='/image/Overwatch_circle_logo.svg' width='89px' height='89px' alt='overwatch logo'/>
+            <h1 style={{color: 'white'}}>Overwatch</h1>
+            <h1 style={{color: 'white'}}>Counter</h1>
         </TitleContainer>
         <Card />
 

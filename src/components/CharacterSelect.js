1011ccf085a94f6400662ca75256e4620f791849
Bailey Mejia <baileymejia@Baileys-MBP.lan>
2023-11-19 15:48:06 -0800

    fixed pathing for logo

--- src/components/CharacterSelect.js
+++ src/components/CharacterSelect.js
@@ -6,7 +6,7 @@ export default function CharacterSelect() {
     return (
         <>
         <TitleContainer>
-            <img src='/image/Overwatch_circle_logo.svg' width='89px' height='89px' alt='overwatch logo'/>
+            <img src={process.env.PUBLIC_URL + '/image/Overwatch_circle_logo.svg'} width='89px' height='89px' alt='overwatch logo'/>
             <h1 style={{color: 'white'}}>Overwatch</h1>
             <h1 style={{color: 'white'}}>Counter</h1>
         </TitleContainer>

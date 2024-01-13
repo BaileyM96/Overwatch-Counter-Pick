1daea1fcfc8af56af95e2735e6464116298cfb84
Bailey Mejia <baileymejia@Baileys-MBP.lan>
2024-01-13 14:46:09 -0800

    Bailey fixed logo and title view for Desktop

--- src/components/CharacterSelect.js
+++ src/components/CharacterSelect.js
@@ -1,19 +1,16 @@
 import Card from '../components/Cards/Card';
-import { TitleContainer } from './styled/Containers/Container.styled';
+import { TitleContainer, H1 } from './styled/Containers/Container.styled';
 
 
 export default function CharacterSelect() {
     return (
         <>
         <TitleContainer>
-            <img src={process.env.PUBLIC_URL + '/image/Overwatch_circle_logo.svg'} width='89px' height='89px' alt='overwatch logo'/>
-            <h1 style={{color: 'white'}}>Overwatch</h1>
-            <h1 style={{color: 'white'}}>Counter</h1>
+            <img src={process.env.PUBLIC_URL + '/image/Overwatch_circle_logo.svg'} width='89px' height='89px' padding-bottom='10px' style={{paddingBottom: '20px'}} alt='overwatch logo'/>
+            <H1>Overwatch</H1>
+            <H1>Counter</H1>
         </TitleContainer>
         <Card />
-
-        
-        {/* <Navbar /> */}
         </>
     )
 }

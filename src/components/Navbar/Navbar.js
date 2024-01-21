59d6a94b38cb3234477a9f3e8a47ad55dd71a5ae
Bailey Mejia <baileymejia28@gmail.com>
2024-01-21 12:44:23 -0800

    Bailey fixed Navbar issues for Desktop/Mobile and spacing issues for bottom page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,9 @@
-import { NavContainer, ClassLink, ClassLink1, BackContainer } from "./Navbar.styled";
+import { NavContainer, ClassLink, ClassLink1, BackContainer, ClassName, ClassIcon } from "./Navbar.styled";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGun, faShieldHalved, faMedkit, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { Link } from 'react-scroll';
 
 export default function Navbar({ showGoBack, onGoBack }) {
-
     return (
         <NavContainer>
             {showGoBack ? (
@@ -19,28 +18,21 @@ export default function Navbar({ showGoBack, onGoBack }) {
             ) : (
                 <>
                 <ClassLink as={Link} to="damage" smooth={true}>
-                    <FontAwesomeIcon style={{width: '30px', height: '30px', paddingLeft: '15px'}}  icon={faGun} />
+                    <ClassIcon icon={faGun} />
+                    <ClassName>Damage</ClassName>
                 </ClassLink>
 
-                <Link as={Link} to="damage" smooth={true}>
-                    <h4 style={{paddingRight: '10px'}}>Damage</h4>
-                </Link>
 
                 <ClassLink as={Link} to="tank" smooth={true}>
-                    <FontAwesomeIcon style={{width: '30px', height: '30px'}}  icon={faShieldHalved} />
+                    <ClassIcon icon={faShieldHalved} />
+                    <ClassName>Tank</ClassName>
                 </ClassLink>
 
-                <Link as={Link} to="tank" smooth={true}>
-                    <h4>Tank</h4>
-                </Link>
 
                 <ClassLink as={Link} to="support" smooth={true}>
-                    <FontAwesomeIcon  style={{width: '30px', height: '30px', paddingLeft: '15px'}}  icon={faMedkit} />
+                    <ClassIcon icon={faMedkit} />
+                    <ClassName>Support</ClassName>
                 </ClassLink>
-
-                <Link as={Link} to="support" smooth={true}>
-                    <h4 style={{paddingRight: '15px'}}>Support</h4>
-                </Link> 
                 </>
             )}
         </NavContainer>
--- src/components/styled/GlobalStyle.styled.js
+++ src/components/styled/GlobalStyle.styled.js
@@ -19,7 +19,7 @@ background: -moz-radial-gradient(circle, hsla(221, 51%, 16%, 1) 26%, hsla(0, 0%,
 background: -webkit-radial-gradient(circle, hsla(221, 51%, 16%, 1) 26%, hsla(0, 0%, 0%, 1) 92%);
 /* height: 100vh; */
 margin: 0;
-padding-bottom: 30%;
+padding-bottom: 80px;
 font-family: 'Luckiest Guy', cursive;
 letter-spacing: 1px;
 }
